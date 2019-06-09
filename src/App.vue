0a90a22ee1cbafacdaa02a4bce189330eb899490
Sophie Henry <sophiesophiehenry@googlemail.com>
2019-06-09 15:24:55 +0100

    added todolist in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+
+  },
+  data() {
+    return {
+      todoList: [
+        {
+          id : 1,
+          title: "wash dishes",
+          completed: false
+        },
+        {
+          id : 2,
+          title: "clean room",
+          completed: false
+       }
+      ]
+    }
   }
 }
 </script>
